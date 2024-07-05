512ef0178f7fee328461d627b2aeb9cc91f29308
chetanpatil0 <patilchetan2905@gmail.com>
2024-07-05 13:43:34 +0530

    Add program 2 file

---
+++ E_Program2.java
@@ -0,0 +1,11 @@
+//2.Add two numbers
+class E_Program2{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=40;
+
+        int result = a + b;
+        System.out.print("Result : " + result);
+    }
+}
