1ddc0590aa50c5183e4b842cbf5598c7a3ff6b9a
chetanpatil0 <patilchetan2905@gmail.com>
2024-07-05 13:45:06 +0530

    Add program 7 file

---
+++ E_Program7.java
@@ -0,0 +1,9 @@
+//7.Print Ascii Value of the Character
+class E_Program7{
+    public static void main(String args[])
+    {
+        char Character = 'A';
+        int CharVal = Character;
+        System.out.print("ASCII Value of " + Character + " Is " + CharVal);
+    }
+}
