e2c48528b709e72906caa8ead6d266a94af5ca95
chetanpatil0 <patilchetan2905@gmail.com>
2024-07-12 17:33:03 +0530

    Add Program 37 file

---
+++ E_Program37.java
@@ -0,0 +1,11 @@
+//37.Calculate area of rectangle
+class E_Program37{
+    public static void main(String args[])
+    {
+        float Length =3.5f;
+        float Breadth = 2.3f;
+
+        float AreaOfRect =Length * Breadth;
+        System.out.print("Area Of Rectangle is : "+AreaOfRect);
+    }
+}
