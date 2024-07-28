2f0becb92993dfc0f230f42e24f7c227718938ff
chetanpatil0 <patilchetan2905@gmail.com>
2024-07-28 21:06:15 +0530

    add Program59 File

---
+++ E_Program59.java
@@ -0,0 +1,12 @@
+//Find the factorial of a number
+class E_Program59{
+    public static void main(String args[])
+    {
+        int Number=7;
+        System.out.print("FACTORIAL OF: "+Number+"! =\t"+Number);
+        for(int i=7-1; i>0;i--)
+        {
+            System.out.print(" x "+i);
+        }
+    }
+}
