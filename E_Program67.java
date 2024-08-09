1d3652d125bf5f81c8ec0b94d5e436443ffa4760
chetanpatil0 <patilchetan2905@gmail.com>
2024-08-09 21:15:38 +0530

    add e program67 file

---
+++ E_Program67.java
@@ -0,0 +1,21 @@
+//Sort A Float Array In Ascending And Descending Order Using for loop
+
+class E_Program67{
+    public static void main(String args[])
+    {
+      int [] Array={5,4,8,1,6,3};
+      for(int i=0; i<Array.length; i++)
+      {
+         for(int j=0;j<Array.length;j++)
+         {
+            
+         }
+      }
+      for(int j=0;j<Array.length;j++)
+         {
+            System.out.print(Array[j]);
+         }
+    }
+}
+
+
