97ada4290336e0ad9bb0ac76845729368b0519e0
chetanpatil0 <patilchetan2905@gmail.com>
2024-08-13 21:29:28 +0530

    add E_Program68 file

---
+++ E_Program68.java
@@ -0,0 +1,17 @@
+//Factorial Program in Java Using For Loop
+class E_Program68{
+    public static void main(String args[])
+    {
+        int Marathi = 50;
+        int English = 96;
+        int Math = 78;
+        int Science = 75;
+        int History= 89;
+
+        int Sum = Marathi + English + Math + Science + History;
+        float Per =Sum / 5;
+
+        System.out.print(Per);
+
+    }
+}
